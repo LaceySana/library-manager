ea1d7e3c984553b4c8389db2731419f350170a0a
Rockywell <47404779+Rockywell@users.noreply.github.com>
2026-04-03 17:10:56 -0600

    Moved swagger documentation to routes
    Refactor routes to include Swagger documentation for each endpoint.

--- routes/books.js
+++ routes/books.js
@@ -1,11 +1,72 @@
 const router = require("express").Router();
-const bookController = require("../controllers/books");
+const { bookController } = require("../controllers");
 // const validator = require("../middleware/validate");
 
-router.get("/", bookController.getAll);
-router.get("/:id", bookController.get);
-router.post("/", /* validator.createBook, */ bookController.create);
-router.put("/:id", /* validator.updateBook, */ bookController.update);
-router.delete("/:id", bookController.delete);
+router.get(
+  "/",
+  /* #swagger.description = 'Get all books.'
+     #swagger.tags = ['books']
+  */
+  bookController.getAll
+);
+router.get(
+  "/:id", 
+  /* #swagger.description = 'Get one book by ID.'
+     #swagger.tags = ['books']
+  */
+  bookController.get
+);
+
+router.post(
+  "/", 
+  /* #swagger.description = "Create a new book."
+     #swagger.tags = ["books"]
+     #swagger.parameter["body"] = {
+        in: 'body',
+        schema: {
+            $title: "",
+            $authorId: "",
+            publishYear: "",
+            $isbn: "",
+            genre: "",
+            image: "",
+            awards: "",
+            $copiesOwned: "",
+            $copiesAvailable: ""
+        }
+    } 
+  */
+  /* validator.createBook, */
+  bookController.create
+);
+router.put(
+  "/:id",
+  /* #swagger.description = "Update book by ID."
+     #swagger.tags = ["books"]
+     #swagger.parameter["body"] = {
+        in: 'body',
+        schema: {
+            $title: "",
+            $authorId: "",
+            publishYear: "",
+            $isbn: "",
+            genre: "",
+            image: "",
+            awards: "",
+            $copies owned: "",
+            $copies available: ""
+        }
+    }
+  */
+  /* validator.updateBook, */
+  bookController.update
+);
+router.delete(
+  "/:id", 
+  /* #swagger.description = 'Delete book by ID.'
+     #swagger.tags = ['books']
+  */
+  bookController.delete
+);
 
 module.exports = router;
