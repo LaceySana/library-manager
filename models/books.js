035d1d1657b468f70da8f4f3321b0b03cfadebf2
Yeva04 <favouryeva24@gmail.com>
2026-04-07 10:54:15 +0100

    Added debug routes to swagger

--- models/books.js
+++ models/books.js
@@ -15,7 +15,7 @@ const bookSchema = new mongoose.Schema({
     isbn: {
         type: String,
         required: true,
-        unigue: true
+        unique: true
     },
     genre: {
         type: String
